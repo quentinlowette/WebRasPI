cd480dd5d102cc68505ffe5a34cbb02ea66db74a
Quentin Lowette <quentin.lowette@gmail.com>
2019-01-17 23:16:25 +0000

    Correction links inside the sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <div class="sidebar">
-	<a class="menuItem" href="index.php">Dashboard</a>
-	<a class="menuItem" href="files.php">Files</a>
+	<a class="menuItem" href="/WebRasPI/index.php">Dashboard</a>
+	<a class="menuItem" href="/WebRasPI/files.php">Files</a>
 	<a class="menuItem" href="/phpmyadmin">PHPMyAdmin</a>
 </div>
